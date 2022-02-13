255ff45dab858509adb2d3f4c65677ac2e3c33a0
aleks <gol8an@yandex.ru>
2022-02-13 17:30:13 +0300

    homework lessons1 new

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,56 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    private int size = 0;
 
     void clear() {
+        Arrays.fill(storage, 0, size, null);
     }
 
     void save(Resume r) {
+        if (r.uuid.equals(r.uuid)) {
+            System.out.println("Резюме есть в базе");
+        } else if (size == storage.length) {
+            System.out.println("Переполнена база");
+        } else {
+            storage[size] = r;
+            size++;
+        }
     }
 
     Resume get(String uuid) {
+        for (Resume rez: storage) {
+            if (rez.uuid.equals(uuid)) {
+                return rez;
+            }
+        }
         return null;
     }
 
     void delete(String uuid) {
+        int index = 0;
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                index = i;
+            }
+            storage[index] = storage[size - 1];
+            storage[size - 1] = null;
+            size--;
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        return Arrays.copyOfRange(storage, 0, size());
     }
 
     int size() {
-        return 0;
+        return size;
     }
 }
