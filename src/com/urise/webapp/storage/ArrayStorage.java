69ef9d5f08077a3007f46609ea17e0c17fca48fc
aleks <gol8an@yandex.ru>
2022-02-25 20:02:31 +0300

    убрал дублирование кода

--- src/com/urise/webapp/storage/ArrayStorage.java
+++ src/com/urise/webapp/storage/ArrayStorage.java
@@ -16,44 +16,37 @@ public class ArrayStorage {
     }
 
     public void save(Resume r) {
-        for (int i = 0; i < size; i++) {
-            if (storage[i].getUuid().equals(r.getUuid())) {
-                System.out.println("Резюме есть в базе" + r.getUuid());
-            } else if (size == storage.length) {
-                System.out.println("Переполнена база" + size);
-            } else {
-                storage[size] = r;
-                size++;
-            }
+        int index = getIndex(r.getUuid());
+        if (index != -1) {
+            System.out.println("Резюме есть в базе" + r.getUuid());
+        } else if (size == storage.length) {
+            System.out.println("Переполнена база" + size);
+        } else {
+            storage[size] = r;
+            size++;
         }
-
     }
 
     public Resume get(String uuid) {
-        for (int i = 0; i < size; i++) {
-            if (storage[i].getUuid().equals(uuid)) {
-                return storage[i];
-            }
-                System.out.println("резюме не найдено в базе" + uuid);
-
+        int index = getIndex(uuid);
+        if (index == -1) {
+            System.out.println("резюме не найдено в базе");
+            return null;
         }
-        return null;
+
+        return storage[index];
     }
 
     public void delete(String uuid) {
-        for (int i = 0; i < size; i++) {
-            if (storage[i] != null && storage[i].getUuid().equals(uuid)) {
-                storage[i] = storage[size - 1];
-                storage[size - 1] = null;
-                size--;
-                break;
-            }
-                System.out.println("резюме не найдено в базе" + uuid);
-                break;
-            }
-
+        int index = getIndex(uuid);
+        if (index == -1) {
+            System.out.println("резюме не найдено в базе");
+        } else {
+            storage[index] = storage[size - 1];
+            storage[size - 1] = null;
+            size--;
         }
-
+    }
 
     /**
      * @return array, contains only Resumes in storage (without null)
@@ -75,5 +68,14 @@ public class ArrayStorage {
                 System.out.println("резюме нет в базе" + resume.getUuid());
             }
         }
+    }
+
+    public int getIndex(String index) {
+        for (int i = 0; i < size; i++) {
+            if (storage[i].getUuid().equals(index)) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
